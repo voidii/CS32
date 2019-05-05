c94caaea8a419f316161a5e2d9cada015c321c33
Ruoqian Zhang <31377103+voidii@users.noreply.github.com>
2019-05-05 16:07:00 -0700

    Add files via upload

--- table.cpp
+++ table.cpp
@@ -1,20 +1,27 @@
-#include <iostream>
-#include <vector>
 #include "table.h"
+#include "entry.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-table::Table(unsigned int max_entries = 100)
+Table::Table() {
+	size_of_table = 100;
+	data_of_table = new vector<Entry>[100];
+}
+
+Table::Table(unsigned int max_entries = 100)
 {
 	size_of_table = max_entries;
 //	this->array_of_vector = new vector<Entry>(100);
-	data_of_table = new vector<Entry> [size_of_table1];
+	data_of_table = new vector<Entry>[size_of_table1];
 
 }
 
-table::Table(unsigned int entries, std::istream& input)
+Table::Table(unsigned int entries, std::istream& input)
 {
-	size_of_table = entries*3;
+	size_of_table = entries*2;
 //	new vector<Entry>* htable(entries);
 	data_of_table = new vector<Entry>[size_of_table];
 	Entry temp;
@@ -50,7 +57,7 @@ void Table::put(unsigned int key, std::string data)
 	}
 }
 
-void table::put(Entry e)
+void Table::put(Entry e)
 {
         int t = 1;
         int index_of_data = Hashfuncs(e.get_key());
@@ -69,7 +76,7 @@ void table::put(Entry e)
 
 }
 
-std::string table::get(unsigned int key) const
+std::string Table::get(unsigned int key) const
 {
 	for(int i = 0; i < data_of_table[Hashfuncs(key)].size(); i++)
         {
@@ -82,68 +89,66 @@ std::string table::get(unsigned int key) const
 	return "";
 }
 
-bool table::remove(unsigned int key)
+bool Table::remove(unsigned int key)
 {
 	for(int i = 0; i < data_of_table[Hashfuncs(key)].size(); i++)
+    {
+        if(data_of_table[index_of_data][i].get_key() == key)
         {
-                if(data_of_table[index_of_data][i].get_key() == key)
-                {
-                        data_of_table[index_of_data][i].erase(index_of_data][i].begin() + i);
-                        return true;
-                }
+            data_of_table[index_of_data][i].erase(index_of_data][i].begin() + i);
+            return true;
         }
+    }
         return false;
 }
 
-void Table:: switch_piv(Entry a[], int size, int& pivotIndex)
+void switch_piv(Entry a[], int size, int& pivotIndex)
 {
- 	 Entry pivot = a[0];
- 	 int left = 1;		 
- 	 int right = size - 1; 
- 	 Entry temp;
+ 	Entry pivot = a[0];
+ 	int left = 1;		 
+ 	int right = size - 1; 
+ 	Entry temp;
 
- 	 while (left <= right)
-	 {
-   		 while (left < size && a[left] <= pivot)
-		 {
-   		   left++;
-  		 }
+ 	while (left <= right)
+	{
+   		while (left < size && a[left] <= pivot)
+		{
+   		  left++;
+  		}
 
-  	 	 while (a[right] > pivot)
-		 {
-		      right--;
-	   	 }
+  	 	while (a[right] > pivot)
+		{
+		     right--;
+	   	}
 
     
-   		 if (left < right)
-		 {
-     			 temp = a[left];
-     			 a[left] = a[right];
-     			 a[right] = temp;
-   		 }
-  	}
-
- 
- 	 pivotIndex = right;
- 	 temp = a[0];
- 	 a[0] = a[pivotIndex];
- 	 a[pivotIndex] = temp;
+   		if (left < right)
+		{
+     			temp = a[left];
+     			a[left] = a[right];
+     			a[right] = temp;
+   		}
+    }
+ 	pivotIndex = right;
+ 	temp = a[0];
+ 	a[0] = a[pivotIndex];
+ 	a[pivotIndex] = temp;
 }
 
 void quicksort(Entry a[], int size)
 {
-	 int pivotIndex;
- 	 int leftSize;		
- 	 int rightSize;		
+	int pivotIndex;
+ 	int leftSize;		
+ 	int rightSize;		
 
- 	 if (size > 1) 
-	 {
-   		 partition(a, size, pivotIndex);
-   		 leftSize = pivotIndex;
-   		 rightSize = size - leftSize - 1;
-   		 quicksort(a, leftSize);
-   		 quicksort((a + pivotIndex + 1), rightSize);
- 	 }
+ 	if (size > 1) 
+	{
+   		partition(a, size, pivotIndex);
+   		leftSize = pivotIndex;
+   		rightSize = size - leftSize - 1;
+   		quicksort(a, leftSize);
+   		quicksort((a + pivotIndex + 1), rightSize);
+ 	}
 }
 
 ostream& operator<< (ostream& out, const Table& t)
@@ -161,10 +166,10 @@ ostream& operator<< (ostream& out, const Table& t)
  	sort(a, count);
 
   	for (int i = 0; i < count; i++)
-       	{
-      		out<<a[i]<<endl;
+    {
+		out<<a[i]<<endl;
  	} 
- 	 return out;
+ 	return out;
 }
 
 	
--- table.h
+++ table.h
@@ -4,9 +4,10 @@
 //
 #ifndef table_h
 #define table_h
-#include <vector>
 #include "entry.h"
-#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
 
 // Note: Your hash table must be implemented using an array of
 // std::vector<Entry> as stated in the instructions.
@@ -14,21 +15,22 @@
 class Table
 {
 	public:
+		Table();
 		Table(unsigned int max_entries = 100);
 		Table(unsigned int entries, std::istream& input);
-		~Table();
 		void put(unsigned int key, std::string data);
   		void put(Entry e);
 		std::string get(unsigned int key) const;
 		bool remove(unsigned int key);
-		std::ostream& operator<< (std::ostream& out, const Table& t);
+		friend std::ostream& operator<< (std::ostream& out, const Table& t);
 
 	private:
-		int size_of_table;
+		unsigned int size_of_table;
 		std::vector<Entry>* data_of_table;
-	        int HashFuncs(int key);	
+	    unsigned int HashFuncs(int key);	
 };
-
+void quicksort(Entry a[], int size);
+void switch_piv(Entry a[], int size, int& pivotIndex);
 
 
 
